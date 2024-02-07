05eb0de8bcdadfb2cf9e2c9b540eced75aae18f2
thanhnguyenduc <nguyenducthanh240903@gmail.com>
2024-02-07 17:43:17 +0700

    fix: inject api url in the source code

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -4,7 +4,7 @@ import { jwtDecode } from "jwt-decode";
 import { logout, setCredential, storeTokens } from "src/redux/state/reducers/authReducer";
 
 const baseApiSlice = fetchBaseQuery({
-  baseUrl: import.meta.env.VITE_APP_API_URL || "http://it-job-portal.thanhnguyen03.site:8080",
+  baseUrl: "http://it-job-portal.thanhnguyen03.site:8080",
   prepareHeaders: (headers) => {
     const token = localStorage.getItem("accessToken");
     if (token) {
